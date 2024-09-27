7a97267d0f704f42841c9acded6db5320febdbc5
Farazros <faraz.rostami@gmail.com>
2024-09-27 19:58:17 +0200

    Adding bottom right panel

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Rectangle;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JButton;
 
 public static void main(String[] args) {
     JPanel mainPanel = new JPanel();
     mainPanel.setPreferredSize(new Dimension(1000,800));
     mainPanel.setBackground(Color.black);
     mainPanel.setLayout(null);
+    mainPanel.add(new BottomRightPanel());
     
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,6 +16,5 @@ public static void main(String[] args) {
     frame.add(mainPanel);
     frame.pack();
     frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    
+    frame.setVisible(true);  
 }
