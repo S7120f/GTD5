968a6efda9c111ac5f41c962e6ba7e487f15a643
Martin Bjervig <martinbjervig@gmail.com>
2024-09-27 20:12:33 +0200

    added BottomPanel as a class

--- Main.java
+++ Main.java
@@ -11,6 +11,8 @@ public static void main(String[] args) {
     mainPanel.setPreferredSize(new Dimension(1000,800));
     mainPanel.setBackground(Color.black);
     mainPanel.setLayout(null);
+    mainPanel.add(new BottomPanel());
+    
     
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
