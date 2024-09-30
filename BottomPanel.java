adc527c29e7e416916e3261cc1b178689e2a22bb
Martin Bjervig <martinbjervig@gmail.com>
2024-09-30 13:03:23 +0200

    added items to the botom-panel

--- BottomPanel.java
+++ BottomPanel.java
@@ -1,27 +1,49 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class BottomPanel extends JPanel {
 public BottomPanel() {
 setBounds(0, 600, 800, 200);
 setBackground(Color.YELLOW);
+setLayout(new FlowLayout(FlowLayout.LEFT));
+
+JTextField toDoTitle = new JTextField("Lägg till titel", 15);  
+toDoTitle.setPreferredSize(new Dimension(100, 30)); // adjust the size of the textfield
+toDoTitle.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));    // textfield border
+toDoTitle.setFont(new Font("Arial", Font.BOLD, 15));    // The font settings
+
+JTextArea descriptionArea = new JTextArea("Lägg till information", 5, 20);
+descriptionArea.setPreferredSize(new Dimension(400, 150));  // adjust the size of the textarea
+descriptionArea.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));  // textfield border
+descriptionArea.setFont(new Font("Arial", Font.BOLD, 15));  // The font settings
+descriptionArea.setLineWrap(true);      // makes the line of text stay within the textarea, so it is visible all the time
+descriptionArea.setWrapStyleWord(true);     // makes the words "non-breakable" when the line of text is changing line
+
+
+JButton addButton = new JButton("add ToDo");
+addButton.setPreferredSize(new Dimension(150, 150));    // size of the button
+addButton.setFont(new Font("Arial", Font.BOLD, 15));    // The font settings
+
+add(toDoTitle);
+add(descriptionArea);
+add(addButton);
+
+
+
 
 
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 }
