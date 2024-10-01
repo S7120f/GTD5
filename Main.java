1e13022d17b4cf3085ecb954dd419002e3b30a3d
Kaarin <kaarin.gaming@tutanota.com>
2024-10-01 04:28:56 +0200

    Adding a getter for the todopanel

--- Main.java
+++ Main.java
@@ -3,22 +3,40 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public static void main(String[] args) {
-    JPanel mainPanel = new JPanel();
-    mainPanel.setPreferredSize(new Dimension(1000, 800));
-    mainPanel.setBackground(Color.black);
-    mainPanel.setLayout(null);
-    mainPanel.add(new BottomPanel());
-    mainPanel.add(new TodoPanel());
-    mainPanel.add(new BottomRightPanel());
-    mainPanel.add(new RightPanel());
-   
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setResizable(false);
-    frame.add(mainPanel);
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);  
+public class Main {
 
+    //Declares a global todopanel object.
+    private static TodoPanel todoPanel;
+
+    public static void main(String[] args) {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(1000, 800));
+        mainPanel.setBackground(Color.black);
+        mainPanel.setLayout(null);
+
+        //Initializes the todopanel object.
+        todoPanel = new TodoPanel();
+
+        mainPanel.add(new BottomPanel());
+        mainPanel.add(todoPanel);
+        mainPanel.add(new BottomRightPanel());
+        mainPanel.add(new RightPanel());
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
+
+    // A getter for the todopanel object.
+    // Call this by Main.getTodoPanel()
+    public static TodoPanel getTodoPanel() {
+        return todoPanel;
+    }
 }
+
