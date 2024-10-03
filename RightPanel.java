036ea2f72fa7b1e27e59e24c259803bb09a35030
Your Namejonas <skonby_jonas@hotmail.com>
2024-10-03 20:20:40 +0200

    more cleaner code

--- RightPanel.java
+++ RightPanel.java
@@ -13,30 +13,34 @@ public class RightPanel extends JPanel {
 
     public RightPanel() {
         setBounds(800, 0, 200, 600);
-        setBackground(new Color (0x202D67));
+        setBackground(new Color(0x202D67));
         Border blackline = BorderFactory.createLineBorder(Color.black);
         setBorder(blackline);
-
     }
 
-    // Create a method addCompletedTask that adds new "Completed Task" panel.
+    // Add a new "Completed Task" panel
     public void addCompletedTask(String completedTask) {
-        JPanel completedTaskPanel = new JPanel(); // Create a new JPanel called completedTask
-        completedTaskPanel.setBackground(Color.GREEN); // Set the background color of the panel to red
-        completedTaskPanel.setPreferredSize(new Dimension(180, 30)); // Set the preferred size of the panel to 150x20
-        completedTaskPanel.setBorder(new BevelBorder(BevelBorder.RAISED)); // Set the border of the panel to a raised bevel border
-        completedTaskPanel.setBorder(new BevelBorder(BevelBorder.LOWERED)); // Set the border of the panel to a lowered bevel border
-
-        JLabel completedLabel = new JLabel(completedTask); // Create a new JLabel called completedLabel
-        completedTaskPanel.add(completedLabel); // Add the completedLabel to the completedTask panel
-        add(completedTaskPanel, BorderLayout.CENTER); // Add the completedTask panel to the RightPanel at the top center
-        completedLabel.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+        JPanel completedTaskPanel = createCompletedTaskPanel(completedTask);
+        add(completedTaskPanel, BorderLayout.CENTER);
         updateCompletedTasks();
+    }
+
+    // Create a panel for each completed task
+    private JPanel createCompletedTaskPanel(String completedTask) {
+        JPanel taskPanel = new JPanel();
+        taskPanel.setPreferredSize(new Dimension(180, 30));
+        taskPanel.setBackground(Color.green);
+        taskPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+        JLabel taskLabel = new JLabel(completedTask);
+        taskLabel.setFont(new Font("Helvetica neue", Font.PLAIN, 16));
+        taskPanel.add(taskLabel);
+
+        return taskPanel;
     }
 
     private void updateCompletedTasks() {
         int completedAmount = getComponentCount();
         Main.getBottomRightPanel().updateCompletedTasks(completedAmount);
     }
-
 }
